19ab937ea512ced12c23903ad5af754ae4d5f943
aaryanverma28 <aaryanverma2806@gmail.com>
2025-07-20 18:50:22 +0530

    Create a new file

---
+++ python_tutorial/sixth_program.py
@@ -0,0 +1,19 @@
+num = [1, 2, 5, 7]
+
+for val in num:
+    print(val)
+
+tup = (1, 2, 5, 7)
+
+for val in tup:
+    print(val)
+
+str = "verma enterprises"
+
+for char in str:
+    if (char == 's'):
+        print("s found")
+        break
+    print(char)
+else:
+    print("END")
