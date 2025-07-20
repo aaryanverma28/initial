f3c5875e8c774987cfd741d5635e324c8b550b84
aaryanverma28 <aaryanverma2806@gmail.com>
2025-07-21 00:50:01 +0530

    Remove sixth_program.py as it is no longer needed

--- fifth_programe1.py
+++ fifth_programe1.py
@@ -1,13 +1,13 @@
-# nums = [1, 2, 3, 4, 5]
+nums = [1, 2, 3, 4, 5]
 
-# for val in nums:
-#     print(val)
+for val in nums:
+    print(val)
 
 
-# tup = (1, 2, 3, 4, 5)
+tup = (1, 2, 3, 4, 5)
 
-# for val in tup:
-#     print(val)
+for val in tup:
+    print(val)
 
 
 str = "verma enterprises"
@@ -20,3 +20,16 @@ for char in str:
 else:
     print("END")
 
+seq = range(10)
+
+for val in seq:
+    print(val)
+
+for i in range(10): #range(stop)
+    print(i)
+
+for i in range(2,10): #range(start, stop)
+    print(i)
+
+for i in range(1, 100 , 2): #range(start, stop, step)
+    print(i)
