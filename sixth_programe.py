f3c5b0a1eab4bf5919a87e86dc5fc55b97caded0
Aaryan Verma <aaryanverma2806@gmail.com>
2025-08-03 23:49:01 +0530

    Update sixth_programe.py

--- sixth_programe.py
+++ sixth_programe.py
@@ -1,1 +1,21 @@
+def calc_sum(a, b): #paraameters
+   return a + b
 
+sum = calc_sum(12000, 12221) # function call; arduments
+print(sum)
+
+def print_hello():
+    print("Hello, World")
+
+print_hello()
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg(90, 95, 96)
+
+print("vermahome", end="$") #sep = " "
+print("aaryanverma") #end = "\n"
